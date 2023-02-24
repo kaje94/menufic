ccdb367b773ac7620f2f181ae2f6039c0ef287ba
kaje94 <a.kajendran@gmail.com>
2023-02-24 22:16:50 +0530

    Increase playwright test timeout

--- playwright.config.ts
+++ playwright.config.ts
@@ -35,7 +35,7 @@ export default defineConfig({
     webServer: {
         command: "npm run dev",
         reuseExistingServer: !process.env.CI,
-        timeout: 10 * 1000,
+        timeout: 60 * 1000,
         url: "http://localhost:3000",
     },
     /* Opt out of parallel tests on CI. */
