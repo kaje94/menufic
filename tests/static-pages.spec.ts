423acc1322f500539602ee95943ff79c117ecad8
kaje94 <a.kajendran@gmail.com>
2023-02-24 22:47:46 +0530

    Update static page playwright tests

--- tests/static-pages.spec.ts
+++ tests/static-pages.spec.ts
@@ -6,10 +6,10 @@ test.describe.configure({ mode: "serial" });
 test("should render landing page", async ({ page }) => {
     await page.goto(`${process.env.TEST_MENUFIC_BASE_URL}`);
     const themeToggle = page.getByTestId("theme-toggle-button");
-    await expect(page).toHaveScreenshot("landing.png", { fullPage: true, maxDiffPixels: 0.8, timeout: 15000 });
+    await expect(page).toHaveScreenshot("landing.png", { fullPage: true, maxDiffPixelRatio: 0.8 });
 
     await themeToggle.click();
-    await expect(page).toHaveScreenshot("landing-dark.png", { fullPage: true, maxDiffPixels: 8, timeout: 15000 });
+    await expect(page).toHaveScreenshot("landing-dark.png", { fullPage: true, maxDiffPixelRatio: 0.8 });
     await themeToggle.click();
 });
 
@@ -17,16 +17,12 @@ test("should render privacy policy page", async ({ page }) => {
     await page.goto(`${process.env.TEST_MENUFIC_BASE_URL}`);
     await page.click("text=Privacy Policy");
     await expect(page).toHaveURL(`${process.env.TEST_MENUFIC_BASE_URL}/privacy-policy`);
-    await expect(page).toHaveScreenshot("privacy-policy.png", { fullPage: true, maxDiffPixels: 0.7, timeout: 15000 });
+    await expect(page).toHaveScreenshot("privacy-policy.png", { fullPage: true, maxDiffPixelRatio: 0.8 });
 });
 
 test("should render terms and conditions page", async ({ page }) => {
     await page.goto(`${process.env.TEST_MENUFIC_BASE_URL}`);
     await page.click("text=Terms & Conditions");
     await expect(page).toHaveURL(`${process.env.TEST_MENUFIC_BASE_URL}/terms-and-conditions`);
-    await expect(page).toHaveScreenshot("terms-and-conditions.png", {
-        fullPage: true,
-        maxDiffPixels: 0.7,
-        timeout: 15000,
-    });
+    await expect(page).toHaveScreenshot("terms-and-conditions.png", { fullPage: true, maxDiffPixelRatio: 0.8 });
 });
